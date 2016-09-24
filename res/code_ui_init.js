f690c330b015db4591ff46db8dd014649af62328
NAT <nat@magicnat.com>
2016-09-24 01:56:20 -0400

    added eval code section.

--- res/code_ui_init.js
+++ res/code_ui_init.js
@@ -1,4 +1,5 @@
 var userCodeMirror = CodeMirror.fromTextArea(document.getElementById("code"), { mode:  "javascript", viewportMargin: Infinity, lineNumbers: true });
+var evalCode = CodeMirror.fromTextArea(document.getElementById("evalCode"), { mode:  "javascript", viewportMargin: Infinity, lineNumbers: true });
 var respondJsonCodeMirror = CodeMirror.fromTextArea(document.getElementById("lastjson"), { mode:  "javascript", json: true, viewportMargin: Infinity, lineWrapping: true });
 
 updateUserCode();
